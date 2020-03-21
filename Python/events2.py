5f949bb8407949774087972e9b21d1eb45870593
Clayton Cornett <claytoncornett@Claytons-MacBook-Pro-2.local>
2020-03-21 13:27:15 -0500

    Completed Changes

--- Python/events2.py
+++ Python/events2.py
@@ -4,11 +4,11 @@ import subprocess
 
 root = Tk()
 
-def openNetflix():
-	webbrowser.open('http://netflix.com')
+def openTwitch():
+	webbrowser.open('http://twitch.tv')
 
-def openHulu():
-	webbrowser.open('http://hulu.com')
+def openXFL():
+	webbrowser.open('http://home.nflbite.com/')
 
 def openKodi():
 	subprocess.call("/home/pi/Desktop/Kodi.sh")
@@ -22,8 +22,8 @@ def openTorrentSites():
 def openSurfShark():
         subprocess.call("/home/pi/Desktop/SeattleVPN.sh")
         
-def openAmazon():
-        webbrowser.open('amazon.com')
+def openDeluge():
+        webbrowser.open('http://localhost:8112')
 
 def openHockey():
         webbrowser.open('https://nhl66.ir/')
@@ -60,49 +60,41 @@ b1 = Button(root, text="Kodi", image=photo1, command=openKodi)
 b1.grid(row=0, column=0)
 #b1.focus_force()
 
-photo2 = PhotoImage(file = "netflixlogo.gif")
-b2 = Button(root, text="Netflix", image=photo2, command=openNetflix)
+photo2 = PhotoImage(file = "twitchlogo.gif")
+b2 = Button(root, text="Twitch", image=photo2, command=openTwitch)
 b2.grid(row=0, column=1)
 
-photo3 = PhotoImage(file = "hululogo.gif")
-b3 = Button(root, text="Hulu",image=photo3, command=openHulu)
+photo3 = PhotoImage(file = "battlehawkslogo.gif")
+b3 = Button(root, text="XFL",image=photo3, command=openXFL)
 b3.grid(row=1, column=0)
 
-photo4 = PhotoImage(file = "amazonlogo.gif")
-b4 = Button(root, text="Amazon Video", image=photo4, command=openAmazon)
+photo4 = PhotoImage(file = "delugelogo.gif")
+b4 = Button(root, text="Deluge", image=photo4, command=openDeluge)
 b4.grid(row=1, column=1)
-
-photo5 = PhotoImage(file = "surfsharklogo.gif")
-b5 = Button(root, text="VPN", image=photo5, command=openSurfShark)
-b5.grid(row=2, column=0)
-
-photo6 = PhotoImage(file = "tpblogo.gif")
-b6 = Button(root, text="Torrent", image=photo6, command=openTorrentSites)
-b6.grid(row=2, column=1)
 
 photo7 = PhotoImage(file = "blueslogo.gif")
 b7 = Button(root, text="Hockey", image=photo7, command=openHockey)
-b7.grid(row=3, column=0)
+b7.grid(row=2, column=0)
 
 photo8 = PhotoImage(file = "kclogo.gif")
 b8 = Button(root, text="Football", image=photo8, command=openFootball)
-b8.grid(row=3, column=1)
+b8.grid(row=2, column=1)
 
 photo9 = PhotoImage(file = "cardslogo.gif")
 b9 = Button(root, text="Baseball", image=photo9, command=openBaseball)
-b9.grid(row=4, column=0)
+b9.grid(row=3, column=0)
 
 photo10 = PhotoImage(file = "celticslogo.gif")
 b10 = Button(root, text="Basketball", image=photo10, command=openBasketball)
-b10.grid(row=4, column=1)
+b10.grid(row=3, column=1)
 
 photo11 = PhotoImage(file = "ytlogo.gif")
 b11 = Button(root, text="Youtube", image=photo11, command=openYoutube)
-b11.grid(row=5, column=0)
+b11.grid(row=4, column=0)
 
 photo12 = PhotoImage(file = "chromiumlogo.gif")
 b12 = Button(root, text="Chromium", image=photo12, command=openChromium)
-b12.grid(row=5, column=1)
+b12.grid(row=4, column=1)
 
 
 root.mainloop()
